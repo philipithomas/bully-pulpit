1b96a773fbf2fab32b2782e90dd6de7a9e5c7aa2
Philip I. Thomas <mail@philipithomas.com>
2026-07-09 12:02:07 -0700

    Add Bell onboarding delivery state migration

---
+++ src/lib/db/migrations/0011_unknown_logan.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "sms_subscribers" ADD COLUMN "bell_contact_card_processing_at" timestamp with time zone;--> statement-breakpoint
+ALTER TABLE "sms_subscribers" ADD COLUMN "bell_contact_card_sent_at" timestamp with time zone;
